558010f7c63265987f9b1cc6b28646b5fe688afc
Darius Hollard <contact@darius-hollard.com>
2022-11-06 19:05:30 +0100

    construction d'une navbar responsive

---
+++ assets/module/navbar.php
@@ -0,0 +1,39 @@
+<nav class="navbar">
+
+    <a href="#" class="nav-logo">
+        <img>
+        <div class="logo-txt">
+            <p>DARIUS</p>
+            <p>HOLLARD</p>
+        </div>
+    </a>
+
+    <ul class="nav-menu">
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">Accueil</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">Mes projets</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">Mon parcours</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">Me contacter</a>
+        </li>
+
+    </ul>
+
+    <div class="nav-toggle">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+
+</nav>
+
+<div class="nav-overlay"></div>
--- fr/accueil.php
+++ fr/accueil.php
@@ -9,6 +9,9 @@
     </head>
 
     <body>
+
+        <?php include_once ('../assets/module/navbar.php'); ?>
+
         <main>
 
             <div id="sec-home-01">
@@ -53,4 +56,46 @@
 
         </main>
     </body>
+
+    <!-- Script interactions navbar -->
+    <script>
+        const navBar = document.querySelector(".navbar");
+        const hamburger = document.querySelector(".nav-toggle");
+        const navMenu = document.querySelector(".nav-menu");
+        const navOverlay = document.querySelector(".nav-overlay");
+
+        // ajouter ou retirer la classe .active lorsqu'on clique sur nav-toggle
+        hamburger.addEventListener("click", () => {
+            navBar.classList.toggle("active");
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+            navOverlay.classList.toggle("active");
+        })
+
+        // retirer la classe .active lorsqu'on clique sur un lien du menu hamburger
+        document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+            navBar.classList.remove("active");
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+            navOverlay.classList.remove("active");
+        }))
+
+        // déclencher la fonction reset quand on change la taille de la fenêtre
+        window.onresize = reset;
+
+        // retire la classe .active lorsque la fenêtre dépasse les 992px
+        function reset(){
+            if(window.innerWidth >= 992){
+                //laptop ou +
+                navBar.classList.remove("active");
+                hamburger.classList.remove("active");
+                navMenu.classList.remove("active");
+                navOverlay.classList.remove("active");
+            }
+            else {
+                //mobile
+            }
+
+        }
+    </script>
 </html>
