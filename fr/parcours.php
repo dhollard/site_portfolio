837ed78cc256ff0127d022b42a148523935bc575
Darius Hollard <contact@darius-hollard.com>
2023-01-27 14:33:11 +0100

    page parcours : réécriture des textes

--- fr/parcours.php
+++ fr/parcours.php
@@ -58,12 +58,12 @@
                             <div class="mod-toggled">
                                 <span class="mod-separator"></span>
                                 <p class="mod-txt">Lors de mon BTS Communication, j'ai appris le savoir théorique des méthodes passées et actuelles pour communiquer auprès d'une audience ciblée.</p>
-                                <p class="mod-txt">Mon diplôme de Designer m’a apporté les capacités techniques complémentaires et nécessaires à la création de contenus visuels comme un site web ou une illustration. Entre autres, les savoir-faire suivants :</p>
+                                <p class="mod-txt">Mon diplôme de Designer m’a apporté les compétences techniques nécessaires à la création de contenus visuels. Quelques exemples :</p>
                                 <ul>
+                                    <li>Réalisation de sites web (HTML / CSS)</li>
+                                    <li>Création d’illustrations et retouches photo</li>
+                                    <li>Maîtrise de la Suite Adobe</li>
                                     <li>Règles de typographie, de colorimétrie et de mise en page</li>
-                                    <li>Utilisation de la Suite Adobe</li>
-                                    <li>Bases de code en HTML / CSS</li>
-                                    <li>Créations d’illustrations et photomanipulation</li>
                                 </ul>
                             </div>
                         </div>
@@ -97,11 +97,11 @@
                             <div class="mod-toggled">
                                 <span class="mod-separator"></span>
                                 <p class="mod-txt">Pendant mon BTS Communication, j’ai pu rédiger de nombreux articles pour la partie blog du site Meilleur Mobile.</p>
-                                <p class="mod-txt">Cette activité m’a sensibilisé aux problématiques du contenu web (ton, phrases courtes, référencement...)</p>
-                                <p class="mod-txt">Lors de mon diplôme de Designer Graphique, j’ai effectué un stage chez l’entreprise Papier Fleur et réalisé plusieurs missions dont :</p>
+                                <p class="mod-txt">Cette activité m’a sensibilisé aux problématiques du copywriting (ton, phrases courtes, référencement...).</p>
+                                <p class="mod-txt">Lors de mon diplôme de Designer Graphique, j’ai réalisé plusieurs missions lors d'un stage dans l’entreprise Papier Fleur :</p>
                                 <ul>
-                                    <li>Refonte de l’arborescence du site web</li>
-                                    <li>Mockups et illustrations pour leur e-commerce</li>
+                                    <li>Amélioration de la structure de leur e-commerce</li>
+                                    <li>Mockups et illustrations pour fiches produits</li>
                                     <li>Photographies des produits</li>
                                 </ul>
                             </div>
@@ -132,12 +132,12 @@
 
                         <div class="module-content">
                             <h3 class="mod-title">Études de Motion Design</h3>
-                            <p class="mod-txt">Pour compléter mes diplômes précédents, j’ai passé une année en Licence Professionnelle de Motion Designer.</p>
-                            <p class="mod-txt">J’ai pu y apprendre l’animation 2D et donner vie aux créations numériques que je maîtrisais déjà.</p>
+                            <p class="mod-txt">Suite à mes deux premiers diplômes, j'ai obtenu une Licence Professionnelle de Motion Designer, dans le but d'apporter une nouvelle dimension à mes compétences.</p>
+                            <p class="mod-txt">J'y ai appris l’animation 2D par interpolation, ou comment donner vie aux créations numériques et les mettre au service d'un story telling.</p>
                             <div class="mod-toggled">
                                 <span class="mod-separator"></span>
-                                <p class="mod-txt">Major de promotion de ma classe, cette année m’a permis de solidifier mes acquis et porter mon intérêt pour la création numérique à un niveau inédit.</p>
-                                <p class="mod-txt">Le Motion Design m’a tout de suite passionné par sa capacité à insuffler une touche de vie dans tous les aspects du monde numérique et à raconter une histoire.</p>
+                                <p class="mod-txt">Major de promotion de ma classe, cette année m’a permis de solidifier mes compétences et de découvrir un domaine passionant.</p>
+                                <p class="mod-txt">Le Motion Design était pour moi la pièce manquante du puzzle entre le design graphique et l'univers du développement web.</p>
                             </div>
                         </div>
                         <button class="mod-bottom-bar">
@@ -165,13 +165,13 @@
                         <div class="blob-shape blob-type-07" id="blob-E"></div>
 
                         <div class="module-content">
-                            <h3 class="mod-title">Proxis Développement</h3>
-                            <p class="mod-txt">J’ai travaillé une année en alternance au sein du groupe industriel Proxis Développement dans le cadre de mon diplôme de Motion Design.</p>
-                            <p class="mod-txt">J’ai eu pour mission de refondre ou créer des sites internet du groupe, ainsi que des visuels et animations.</p>
+                            <h3 class="mod-title">Proxis Développement (alternance)</h3>
+                            <p class="mod-txt">J’ai travaillé une année en alternance au sein du groupe industriel Proxis Développement dans le cadre de mon diplôme de Motion Designer.</p>
+                            <p class="mod-txt">J’étais chargé de refondre ou créer de zéro les sites internet du groupe, ainsi que des visuels et animations.</p>
                             <div class="mod-toggled">
                                 <span class="mod-separator"></span>
-                                <p class="mod-txt">J’ai eu l’occasion de prendre en charge des missions variées au cours de mon année d’alternance.</p>
-                                <p class="mod-txt">Travaillant avec beaucoup d’autonomie, en parallèle de mon apprentissage scolaire, cette année m’a permis de me dépasser et confirmer mes capacités dans un cadre professionnel.</p>
+                                <p class="mod-txt">Mes missions furent variées et nombreuses lors de cette année, ce qui m'a permis d'apprendre et progresser.</p>
+                                <p class="mod-txt">Travaillant avec beaucoup d’autonomie, en parallèle de mon apprentissage scolaire, cette expérience m’a permis de me dépasser et confirmer mes capacités dans un cadre professionnel.</p>
                             </div>
                         </div>
                         <button class="mod-bottom-bar">
@@ -199,13 +199,14 @@
                         <div class="blob-shape blob-type-08" id="blob-F"></div>
 
                         <div class="module-content">
-                            <h3 class="mod-title">DataOptique / Welwot</h3>
+                            <h3 class="mod-title">DataOptique / Welwot (CDI)</h3>
                             <p class="mod-txt">Depuis 2020, je travaille en tant que Chef de projet web designer au sein de l’entreprise DataOptique et sa société jumelle Welwot.</p>
-                            <p class="mod-txt">Recruté initialement pour la partie webdesign, j’ai rapidement apporté mes autres compétences sur la table, prenant aussi en charge toute la partie de communication visuelle et de gestion de certains projets de l’entreprise.</p>
+                            <p class="mod-txt">Recruté initialement pour la création de sites web, j’ai rapidement pu exercer mes autres compétences, en prenant en charge la communication visuelle et la gestion de projets.</p>
                             <div class="mod-toggled">
                                 <span class="mod-separator"></span>
-                                <p class="mod-txt">Les besoins de ces deux entreprises ont poussé mon approche de l’UI/UX plus loin. J’ai dû concevoir des interfaces webs complexes, comme des tableaux de bord pouvant rivaliser avec une application mobile dédiée.</p>
-                                <p class="mod-txt">J'ai également modernisé la conduite de projets de développements, en y introduisant l'usage de GIT. Ainsi qu'en plaidant pour l'organisation de stand-up meetings hebdomadaires et d'un outil de gestion dédié.</p>
+                                <p class="mod-txt">L'ampleur des missions qui m'ont été confiés ont nécessité une approche de l’UI/UX plus poussée que mes travaux passées.</p>
+                                <p class="mod-txt">Lors de ces missions, j'ai conçu des architectures web complexes, comme des tableaux de bord et des interfaces utilisateur accessibles et modernes.</p>
+                                <p class="mod-txt">J'ai également amélioré la conduite de projets de développements, en y introduisant l'usage de GIT. Ainsi qu'en instorant des "stand-up meetings" hebdomadaires et l'usage d'outils de gestion dédiés.</p>
                             </div>
                         </div>
                         <button class="mod-bottom-bar">
@@ -223,26 +224,26 @@
             </div>
 
             <div class="timeline-end">
-                <h2 class="end-big-title">Aujourd'hui</h2>
+                <h2 class="end-big-title">Au fil du temps</h2>
 
                 <div class="end-module">
                     <div class="blob-shape blob-type-03" id="blob-G"></div>
                     <div class="blob-shape blob-type-02" id="blob-H"></div>
 
-                    <h3 class="end-title">Auto-apprentissage et projets personnels</h3>
+                    <h3 class="end-title">Auto-formation et projets personnels</h3>
 
-                    <p class="end-txt">Je continue d’explorer les nombreux horizons que propose le domaine de la création numérique, en me formant sur de nouveaux outils et en réalisant des projets.</p>
+                    <p class="end-txt">En parallèle de mes activités professionelles, je me tiens informé sur les nouveautés du domaine de la création numérique. Je suis toujours à la recherche de nouveaux outils me permettant de créer plus efficacement et avec plus de liberté.</p>
 
-                    <p class="end-txt">Une liste non-exaustive de domaines de compétences ayant piqué mon intérêt :</p>
+                    <p class="end-txt">Travaillant dans un domaine en constante évolution, je prend part régulièrement à des formations en ligne pour acquérir de nouvelles compétences.</p>
+
+                    <p class="end-txt">Quelques exemples de savoir-faires que je souhaite apprendre :</p>
 
                     <ul>
                         <li>Modélisation et animation 3D</li>
                         <li>Création de musiques et de bruitages</li>
-                        <li>Sketching rapide pour matérialiser une idée et mieux communiquer en équipe</li>
+                        <li>Techniques de croquis avancées pour mieux communiquer en équipe</li>
                         <li>Outils d'intelligence artificielle appliquée</li>
                     </ul>
-
-                    <p class="end-txt">Quant à mes projets, je vous invite à les découvrir sur la page qui leur est consacrée :</p>
 
                     <a href="/fr/projets.php" class="end-btn">Voir mes projets</a>
                 </div>
