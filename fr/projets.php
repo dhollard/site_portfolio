bbd5a062994b815fb4e1299c69f07b03aec44d80
Darius Hollard <contact@darius-hollard.com>
2023-02-26 19:06:37 +0100

    mise à jour photo accueil et CV + construction page mes projets temporaire

--- fr/projets.php
+++ fr/projets.php
@@ -1,4 +1,64 @@
-<!-- navbar -->
-<?php include_once ('../assets/module/navbar.php'); ?>
+<!doctype html>
+<html lang="fr">
 
-En construction
+<head>
+    <!-- Balises communes -->
+    <?php include_once('../assets/module/head-module.php'); ?>
+
+    <!-- Appels ressources page -->
+    <link rel="stylesheet" href="/assets/css/style-projets.css">
+
+    <!-- SEO et identité page -->
+    <link rel="canonical" href="https://www.darius-hollard.com/fr/projets.php" />
+    <title>Mes projets</title>
+    <meta name="description" content="" />
+
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <?php include_once('../assets/module/navbar.php'); ?>
+
+    <main>
+
+        <!-- Section 01 -->
+        <section id="sec-proj-01">
+
+            <!--  Bandeau titre -->
+            <div class="page-header">
+                <h1>Mes projets</h1>
+            </div>
+
+            <div class="projet-select">
+
+                <!--<div class="projet-bar">
+                    <div class="content-left">
+                        <h2>Titre du projet</h2>
+                        <p>Texte du paragraphe</p>
+                        <a href="#">Voir ce projet</a>
+                    </div>
+                        
+                    <img src="/assets/img/portrait-dh.jpg">
+                </div>-->
+
+                <div class="projet-bar">
+                    <h2 style="text-align:center; width:fit-content; margin:auto; padding:20px; font-size:38px;">En construction</h2>
+                </div>
+
+            </div>
+
+        </section>
+
+
+    </main>
+
+    <!-- Footer -->
+    <?php include_once('../assets/module/footer.php'); ?>
+
+    <!-- Appels et scripts de fin de page -->
+    <?php include_once('../assets/module/end-page-module.php'); ?>
+
+</body>
+
+</html>
