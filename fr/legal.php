63177bf78889195efa6ec4ac7eaa2cad90a57dc5
Darius Hollard <contact@darius-hollard.com>
2023-03-09 22:15:33 +0100

    finalisation de la page mentions légales

--- fr/legal.php
+++ fr/legal.php
@@ -28,20 +28,42 @@
             <h1>Mentions légales</h1>
 
             <div class="legal-bloc">
-                <h2 class="bloc-title">Éditeur du présent site internet</h2>
+                <h2 class="bloc-title">Éditeur et gestionnaire</h2>
                 <p>Ce site internet a été créé par M. Darius Hollard dans le but d'avoir une vitrine en ligne personnelle de ses travaux.</p>
                 <p>Ce site est directement géré et mis à jour par M. Darius Hollard, sans autres intermédiaires.</p>
                 <p>M. Darius Hollard est joignable via le formulaire de contact de ce site, ou directement à l'adresse e-mail "contact@darius-hollard.com".</p>
-                <p>M. Darius Hollard ne souhaite pas divulger ses autres coordonnées personnelles, qui sont cependant disponibles auprès de l'hébergeur du présent site internet, dans le cadre d'une procédure judiciaire théorique.</p>
+                <p>M. Darius Hollard ne souhaite pas divulger ses autres coordonnées personnelles, qui peuvent cependant être rendues disponibles en contactant l'hébergeur du présent site internet, dans le cadre d'une procédure judiciaire théorique uniquement.</p>
             </div>
 
             <div class="legal-bloc">
-                <h2 class="bloc-title">Titre de bloc</h2>
-                <p class="bloc-text">Paragraphe de texte de bloc</p>
+                <h2 class="bloc-title">Hébergeur</h2>
+                <p>Le présent site internet est hébergée par l'entreprise IONOS.</p>
+                <p>Raison sociale : IONOS SARL</p>
+                <p>Téléphone service client : 0970 808 911 (appel non surtaxé)</p>
+                <p>E-mail de contact : info@ionos.fr</p>
+                <p>Pour plus d'informations sur l'hébergeur, merci de vous rendre sur leur site internet et de les contacter directement.</p>
+                <p>Site de l'hébergeur : <a class="bloc-link" href="https://www.ionos.fr/" target="_blank">https://www.ionos.fr/</a></p>
+            </div>
+
+            <div class="legal-bloc">
+                <h2 class="bloc-title">Propriété intellectuelle</h2>
+                <p>M. Darius Hollard est le propriétaire du présent site internet et de tout le contenu qu'il contient.</p>
+                <p>Lorsqu'un projet mis en avant sur ce site inclu la participation d'autres personnes ou entreprise au sein desquelles M. Darius Hollard a travaillé, leur nom et rôle dans le projet est mentionné de manière la plus précise possible.</p>
+                <p>Les éléments protégées par cette propriété intellectuelle sont :</p>
                 <ul class="bloc-list">
-                    <li>Liste à puce</li>
+                    <li>Le code source du présent site internet</li>
+                    <li>Les graphiques, illustrations, animations, logos ou photographies présentes sur ce site</li>
+                    <li>Le contenu textuel présent sur ce site et n'étant pas issu d'une citation externe provenant d'un autre contenu</li>
                 </ul>
-                <a class="bloc-link">Lien</a>
+
+                <p>Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de M. Darius Hollard.</p>
+                    
+                <p>Cette représentation ou reproduction, par quelques procédés que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle.</p>
+                    
+                <p>Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des contenus copiés pourraient intenter une action en justice à votre encontre.</p>
+
+                <p>M. Darius Hollard est identiquement propriétaire des "droits des producteurs de bases de données" visés au Livre III, Titre IV, du Code de la Propriété Intellectuelle (loi n° 98-536 du 1er juillet 1998) relative aux droits d'auteur et aux bases de données.</p>
+
             </div>
             
 
