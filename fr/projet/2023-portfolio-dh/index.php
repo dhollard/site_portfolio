459e9ac9d5befdc51717acc2c1ba5122dccb43a8
Darius Hollard <contact@darius-hollard.com>
2023-06-23 21:48:50 +0200

    finalisation de la section showreel des pages projets, avancement section objectifs

--- fr/projet/2023-portfolio-dh/index.php
+++ fr/projet/2023-portfolio-dh/index.php
@@ -72,23 +72,65 @@
 
             </section>
 
+            <!-- ********** Section "showreel" ********** -->
+            <section id="Showreel_projet" class="section-projet">
+                <div class="section-ligne">
+
+                    <div class="full-col">
+
+                        <figure class="bloc-cadre">
+                            <div class="cadre-mobile cadre-video">
+                            <video class="bloc-video" width="100%" height="auto" autoplay loop muted>
+                                <source src="video/showreel-mobile.webm" type="video/webm">
+                                <source src="video/showreel-mobile.mp4" type="video/mp4">
+                                Votre navigateur ne peut pas lire cette vidéo.
+                            </video>
+                            </div>
+
+                            <div class="cadre-desktop cadre-video">
+                            <video class="bloc-video" width="100%" height="auto" autoplay loop muted>
+                                <source src="video/showreel-desktop.webm" type="video/webm">
+                                <source src="video/showreel-desktop.mp4" type="video/mp4">
+                                Votre navigateur ne peut pas lire cette vidéo.
+                            </video>
+                            </div>
+
+                            <figcaption class="legende-cadre">Démo du site <a href="https://darius-hollard.com/" target="_blank">darius-hollard.com</a>, scrollez pour voir son processus de création</figcaption>
+
+                        </figure>
+
+                    </div>
+
+                    
+                    
+                </div>
+            </section>
+
             <!-- ********** Section "objectifs" ********** -->
-            <section class="section-projet objectifs-projet">
-                <div class="demi-col">
-                    <h2 class="titre-section">02 - Objectifs</h2>
+            <section id="Objectifs_projet" class="section-projet">
+                <div class="section-ligne">
 
-                    <div class="bloc-texte">
-                    <p>J’ai débuté la création de ce site pour me permettre d’afficher mes projets et mon parcours sur internet.</p>
+                    <div class="section-colonne demi-col">
 
-                    <p>Son rôle principal est de me permettre de partager ce contenu rapidement avec n’importe qui.</p>
+                        <h2 class="titre-section">02 - Objectifs</h2>
 
-                    <p>Le site portfolio terminé devra remplir le rôle d’un support de présentation esthétique et soigné.</p>
+                        <div class="bloc-texte">
+                            <p>J’ai débuté la création de ce site pour me permettre d’afficher mes projets et mon parcours sur internet.</p>
 
-                    <p>Il servira aussi d’exemple concret de mon savoir-faire mis en application.</p>
+                            <p>Son rôle principal est de me permettre de partager ce contenu rapidement avec n’importe qui.</p>
+
+                            <p>Le site portfolio terminé devra remplir le rôle d’un support de présentation esthétique et soigné.</p>
+
+                            <p>Il servira aussi d’exemple concret de mon savoir-faire mis en application.</p>
+                        </div>
+
+                    </div>
+                    
+                    <div class="demi-col">
+                        <img src="img/schema-objectifs.png" width="100%">
+                    </div>
+
                 </div>
-                </div>
-                
-
 
             </section>
 
