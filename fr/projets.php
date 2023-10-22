329f2afda28175419a126be469c3d264b89f83ff
Darius Hollard <contact@darius-hollard.com>
2023-10-22 17:37:23 +0200

    Ajouter d'un système de filtrage des projets via une barre en haut de la page 'Mes projets'

--- fr/projets.php
+++ fr/projets.php
@@ -30,10 +30,36 @@
                 <h1>Mes projets</h1>
             </div>
 
+            <!-- Bandeau de filtrage -->
+            <div class="wrapper-filtre-projet">
+                <div class="zone-01">
+
+                    <div id="Filtre_proj_categorie" class="bloc-filtre">
+                        <label class="filtre-label">Trier par domaine d'expertise</label>
+                        <div class="filtre-btn-bloc">
+                            <button class="cat active-filter" data-cat="tout">Tout</button>
+                            <button class="cat" data-cat="web">Web design</button>
+                            <button class="cat" data-cat="motion">Motion design</button>
+                            <button class="cat" data-cat="design">Graphisme</button>
+                        </div>
+                    </div>
+
+                    <div id="Filtre_proj_type" class="bloc-filtre">
+                        <label class="filtre-label">Trier par format de projet</label>
+                        <div class="filtre-btn-bloc">
+                            <button class="type active-filter" data-type="tout">Tout</button>
+                            <button class="type" data-type="etude">Étude de cas</button>
+                            <button class="type" data-type="resume">Résumé</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
             <div class="grid-wrapper">
                 <div class="projet-grid">
 
-                    <a id="Proj_web_site_portfolio_DH_2023" class="projet-bloc" href="/fr/projet/2023-portfolio-dh/">
+                    <a id="Proj_web_site_portfolio_DH_2023" class="projet-bloc" href="/fr/projet/2023-portfolio-dh/" data-cat="web" data-type="etude">
                         <h2 class="projet-titre">Site portfolio</h2>
 
                         <p class="projet-date">2023</p>
@@ -46,7 +72,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_web_site_welwot_2021" class="projet-bloc" href="/fr/projet/2021-site-welwot/">
+                    <a id="Proj_web_site_welwot_2021" class="projet-bloc" href="/fr/projet/2021-site-welwot/" data-cat="web" data-type="resume">
                         <h2 class="projet-titre">Site internet</h2>
 
                         <p class="projet-date">2021</p>
@@ -59,7 +85,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_web_site_wotbus_2022" class="projet-bloc" href="/fr/projet/2022-site-wotbus/">
+                    <a id="Proj_web_site_wotbus_2022" class="projet-bloc" href="/fr/projet/2022-site-wotbus/" data-cat="web" data-type="resume">
                         <h2 class="projet-titre">Site internet</h2>
 
                         <p class="projet-date">2022</p>
@@ -72,7 +98,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_web_ui_voyageur_wotbus_2022" class="projet-bloc" href="/fr/projet/2022-ui-voyageur-wotbus/">
+                    <a id="Proj_web_ui_voyageur_wotbus_2022" class="projet-bloc" href="/fr/projet/2022-ui-voyageur-wotbus/" data-cat="web" data-type="resume">
                         <h2 class="projet-titre">Interface utilisateur</h2>
 
                         <p class="projet-date">2022/2023</p>
@@ -85,7 +111,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_web_dashboard_wiavis_2022" class="projet-bloc" href="/fr/projet/2022-dashboard-wi-avis/">
+                    <a id="Proj_web_dashboard_wiavis_2022" class="projet-bloc" href="/fr/projet/2022-dashboard-wi-avis/" data-cat="web" data-type="resume">
                         <h2 class="projet-titre">Tableau de bord</h2>
 
                         <p class="projet-date">2022</p>
@@ -98,7 +124,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_web_site_dataoptique_2022" class="projet-bloc" href="/fr/projet/2022-site-dataoptique/">
+                    <a id="Proj_web_site_dataoptique_2022" class="projet-bloc" href="/fr/projet/2022-site-dataoptique/" data-cat="web" data-type="resume">
                         <h2 class="projet-titre">Site internet</h2>
 
                         <p class="projet-date">2022</p>
@@ -111,7 +137,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_design_com_wotbus_2023" class="projet-bloc" href="/fr/projet/2023-communication-wotbus/">
+                    <a id="Proj_design_com_wotbus_2023" class="projet-bloc" href="/fr/projet/2023-communication-wotbus/" data-cat="design" data-type="resume">
                         <h2 class="projet-titre">Communication visuelle</h2>
 
                         <p class="projet-date">2023</p>
@@ -124,7 +150,7 @@
                         </div>
                     </a>
 
-                    <a id="Proj_web_site_cawibox_2020" class="projet-bloc" href="/fr/projet/2020-site-cawibox/">
+                    <a id="Proj_web_site_cawibox_2020" class="projet-bloc" href="/fr/projet/2020-site-cawibox/" data-cat="web" data-type="resume">
                         <h2 class="projet-titre">Site internet</h2>
 
                         <p class="projet-date">2020</p>
@@ -137,19 +163,6 @@
                         </div>
                     </a>
 
-                    <!-- <a id="Proj_teasing" class="projet-bloc">
-                        <h2 class="projet-titre">Prochainement...</h2>
-
-                        <p class="projet-date"></p>
-                        <span class="sepa-info"></span>
-                        <p class="projet-marque"></p>
-                        
-                        <div class="projet-visuel">
-                            <div class="projet-miniature"></div>
-                            <div class="projet-overlay"></div>
-                        </div>
-                    </a> -->
-
                 </div>
             </div>
 
@@ -160,6 +173,58 @@
 
     <!-- Footer -->
     <?php include_once('../assets/module/footer.php'); ?>
+
+    <!-- Script changement des projets affichés en fonction des filtres -->
+    <script>
+        // --- Définir les variables de filtre au chargement ---
+        var catFilter = "tout"; // Valeur par défaut du filtre pour la catégorie
+        var typeFilter = "tout"; // Valeur par défaut du filtre pour le type
+
+        // --- Mettre à jour les variables de filtre au clic d'un bouton ---
+        $(".cat, .type").click(function() {
+            // Récupérer la valeur de catégorie et de type depuis les boutons
+            var clickedCat = $(this).data("cat");
+            var clickedType = $(this).data("type");
+
+            // Vérifier si un bouton cat est cliqué avant de mettre à jour la variable catFilter
+            if ($(this).hasClass("cat")) {
+            catFilter = clickedCat;
+            }
+
+            // Vérifier si un bouton type est cliqué avant de mettre à jour la variable typeFilter
+            if ($(this).hasClass("type")) {
+            typeFilter = clickedType;
+            }
+
+            // --- Afficher les éléments correspondant aux filtres sélectionnés ---
+
+            // Si tous les filtres sont sur la valeur "tout"
+            if (catFilter === "tout" && typeFilter === "tout") {
+            $(".projet-bloc").show();
+            }
+            // Si le filtre de catégorie est sur la valeur "tout"
+            else if (catFilter === "tout") {
+            $(".projet-bloc[data-type='" + typeFilter + "']").show();
+            $(".projet-bloc[data-type!='" + typeFilter + "']").hide();
+            }
+            // Si le filtre de type est sur la valeur "tout"
+            else if (typeFilter === "tout") {
+            $(".projet-bloc[data-cat='" + catFilter + "']").show();
+            $(".projet-bloc[data-cat!='" + catFilter + "']").hide();
+            }
+            // Si acun des filtres n'est sur la valeur "tout"
+            else {
+            $(".projet-bloc").hide();
+            $(".projet-bloc[data-cat='" + catFilter + "'][data-type='" + typeFilter + "']").show();
+            }
+
+            // Ajouter la classe .active-filter sur le bouton cliqué, puis la retirer de tous les boutons du même menu de filtre
+            $(this).addClass("active-filter");
+            $(this).siblings("." + (clickedCat ? "cat" : "type")).removeClass("active-filter");
+        });
+    </script>
+
+
 
     <!-- Appels et scripts de fin de page -->
     <?php include_once('../assets/module/end-page-module.php'); ?>
