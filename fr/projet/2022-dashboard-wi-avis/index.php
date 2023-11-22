3c2b74d134c8ffd64bc1d1985192c1d57d41e6fd
Darius Hollard <contact@darius-hollard.com>
2023-11-22 21:33:42 +0100

    Ajout de la page projet résumé tableau de bord wi-avis + ajustement des règles flex de l'élément .infos-projet

---
+++ fr/projet/2022-dashboard-wi-avis/index.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Balises communes -->
+    <?php include_once('../../../assets/module/head-module.php'); ?>
+
+    <!-- Appels ressources page -->
+    <link rel="stylesheet" href="/assets/css/style-2022-dashboard-wi-avis.css">
+
+    <!-- SEO et identité page -->
+    <link rel="canonical" href="https://darius-hollard.com/fr/projet/2022-dashboard-wi-avis/" />
+    <title>Tableau de bord - Wi-avis</title>
+    <meta name="description" content="Projet réalisé en 2022 - Création d'un tableau de bord pour Wi-avis" />
+
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <?php include_once('../../../assets/module/navbar.php'); ?>
+
+    <main>
+
+        <!-- ********** Section en-tête page projet ********** -->
+        <section id="Header_projet">
+            <h1 id="Titre_page">Tableau de bord Wi-avis</h1>
+            <div id="Scroll_btn_header" onclick="headerScrollButton()">
+                <span class="scroll-btn-header-icon"></span>
+            </div>
+        </section>
+
+        <!-- ********** Section "résumé du projet" ********** -->
+        <section id="Resume_projet" class="section-projet">
+
+            <div class="section-bloc">
+                <div class="section-ligne">
+
+                    <div class="section-colonne full-col">
+
+                        <div class="module-resume">
+
+                            <div class="txt-resume-projet">
+                                <h2 class="titre-section">01 - Résumé du projet</h2>
+
+                                <div class="bloc-texte">
+                                    <p>Pendant ma collaboration au sein de l'entreprise DataOptique, j'ai travaillé plusieurs mois sur un concept nommé Wi-avis. Son objectif était d'apporter aux commerçants un moyen innovant pour interagir avec leurs clients, sans avoir recours à un site ou une application dédiée.</p>
+
+                                    <p>Bien que le projet soit resté à l'état de prototype abandonné, la réflexion autour de son UI/UX a été pour moi une occasion intéressante de progresser.</p>
+
+                                    <p>Le projet comportait beaucoup de contraintes complexes et il a donc fallu redoubler d'ingéniosité pour rendre l'ensemble de ce tableau à la fois ergonomique, mobile-friendly et attirant visuellement.</p>
+
+                                    <p>Cela m'a donné la possibilité de mettre au point méticuleusement toute une panoplie de composants UI, d'articuler leurs interactions et de designer des sets d'icones uniques.</p>
+
+                                    <p>Vous trouverez sur cette page un aperçu du prototype Figma que j'ai réalisé pour ce projet.</p>
+                                </div>
+                            </div>
+
+
+                            <div class="infos-projet">
+                                <span class="bloc-infos-projet">
+                                    <p id="Info_resume_01" class="titre-infos-projet">Type de projet</p>
+                                    <p>UI/UX design</p>
+                                </span>
+
+                                <span class="bloc-infos-projet">
+                                    <p id="Info_resume_02" class="titre-infos-projet">Rôle effectué</p>
+                                    <p>Product Designer</p>
+                                </span>
+
+                                <span class="bloc-infos-projet">
+                                    <p id="Info_resume_03" class="titre-infos-projet">Durée du projet</p>
+                                    <p>2 mois (2021)</p>
+                                </span>
+
+                                <span class="bloc-infos-projet">
+                                    <p id="Info_resume_04" class="titre-infos-projet">Commanditaire</p>
+                                    <p>DataOptique</p>
+                                    <p class="info-projet-secondaire">(spécialiste collecte de données d'enquête)</p>
+                                </span>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="section-bloc">
+
+                <div class="section-ligne">
+
+                    <div class="section-colonne full-col">
+
+                        <figure class="zone-cadre-video">
+
+                            <div class="cadre-scrollable">
+                                <!-- Fausse barre de navigateur -->
+                                <?php include('../../../assets/module/modules-projets/navibar-cadre-desk.php'); ?>
+
+                                <div class="cadre-content-wrapper">
+                                    <!-- Vidéo format mobile -->
+                                    <video id="Video_showreel_mob" class="bloc-video" width="100%" height="auto" autoplay playsinline loop muted>
+                                        <source src="video/video-mobile-page-questionnaires.webm" type="video/webm">
+                                        <source src="video/video-mobile-page-questionnaires.mp4" type="video/mp4">
+                                        Votre navigateur ne peut pas lire cette vidéo.
+                                    </video>
+
+                                    <!-- Vidéo format ordinateur -->
+                                    <video id="Video_showreel_desk" class="bloc-video" width="100%" height="auto" autoplay playsinline loop muted>
+                                        <source src="video/video-page-questionnaires.webm" type="video/webm">
+                                        <source src="video/video-page-questionnaires.mp4" type="video/mp4">
+                                        Votre navigateur ne peut pas lire cette vidéo.
+                                    </video>
+                                </div>
+                            </div>
+
+                            <figcaption class="legende-visuel">Page dédiée à la gestion de questionnaires (exemple issu du prototype Figma du projet).</a></figcaption>
+
+                        </figure>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="section-bloc">
+
+                <div class="section-ligne">
+
+                    <div class="section-colonne full-col">
+
+                        <figure class="zone-cadre-video">
+
+                            <div class="cadre-scrollable">
+                                <!-- Fausse barre de navigateur -->
+                                <?php include('../../../assets/module/modules-projets/navibar-cadre-desk.php'); ?>
+
+                                <div class="cadre-content-wrapper">
+                                    <!-- Vidéo format mobile -->
+                                    <video id="Video_showreel_mob" class="bloc-video" width="100%" height="auto" autoplay playsinline loop muted>
+                                        <source src="video/video-mobile-page-boitiers.webm" type="video/webm">
+                                        <source src="video/video-mobile-page-boitiers.mp4" type="video/mp4">
+                                        Votre navigateur ne peut pas lire cette vidéo.
+                                    </video>
+
+                                    <!-- Vidéo format ordinateur -->
+                                    <video id="Video_showreel_desk" class="bloc-video" width="100%" height="auto" autoplay playsinline loop muted>
+                                        <source src="video/video-page-boitiers.webm" type="video/webm">
+                                        <source src="video/video-page-boitiers.mp4" type="video/mp4">
+                                        Votre navigateur ne peut pas lire cette vidéo.
+                                    </video>
+                                </div>
+                            </div>
+
+                            <figcaption class="legende-visuel">Page dédiée à la gestion des boitiers Wi-avis (exemple issu du prototype Figma du projet).</a></figcaption>
+
+                        </figure>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+    <div id="Overlay_sommaire" class="">
+
+    </div>
+
+    <!-- Pre-footer pages projets -->
+    <?php include_once('../../../assets/module/modules-projets/end-bloc-projet.php'); ?>
+
+    <!-- Footer -->
+    <?php include_once('../../../assets/module/footer.php'); ?>
+
+    <!-- Appels et scripts de fin de page -->
+    <?php include_once('../../../assets/module/end-page-module.php'); ?>
+
+    <!-- Appel scripts spéciaux pages projets -->
+    <?php include_once('../../../assets/module/modules-projets/page-projet-module-script.php'); ?>
+
+</body>
+
+</html>
